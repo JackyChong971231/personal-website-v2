baa4401177d08d8ac899e96ec39e4780f31e3768
Jacky <chongkinye@gmail.com>
2022-10-21 01:50:44 +0800

    added more pics and navbar now equipped with autohide feature

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,35 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import {NavBar} from './NavBar';
 import { HomePage } from './HomePage';
+import React, { useState, useEffect } from 'react';
 
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
+  const [prevScrollpos, setPrevScrollpos] = useState(0);
+
+  useEffect(() => {
+    const onScroll = e => {
+      if (prevScrollpos > window.pageYOffset) {
+        document.getElementsByClassName("navbar")[0].style.top = "0";
+      } else {
+        document.getElementsByClassName("navbar")[0].style.top = "-60px";
+      }
+      setPrevScrollpos(window.pageYOffset);
+    }
+    window.addEventListener("scroll", onScroll)
+
+    // if (prevScrollpos > window.pageYOffset) {
+    //   document.getElementById("navbar").style.top = "0";
+    // } else {
+    //   document.getElementById("navbar").style.top = "-50px";
+    // }
+    // setPrevScrollpos(window.pageYOffset);
+
+  })
+
   return (
     <div className="App">
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,78 +8,13 @@ import { faBars, faBell, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export function NavBar2() {
-    return (
-        <>
-            {/* <!-- Navbar --> */}
-            <nav class="navbar navbar-light bg-light">
-                {/* <!-- Container wrapper --> */}
-                <div class="container">
-                    {/* <!-- Navbar brand --> */}
-                    <a class="navbar-brand me-2" href="https://mdbgo.com/">
-                    <img
-                        src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-                        height="16"
-                        alt="MDB Logo"
-                        loading="lazy"
-                        style="margin-top: -1px;"
-                    />
-                    </a>
-
-                    {/* <!-- Toggle button --> */}
-                    <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-mdb-toggle="collapse"
-                    data-mdb-target="#navbarButtonsExample"
-                    aria-controls="navbarButtonsExample"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                    >
-                    <i class="fas fa-bars"></i>
-                    </button>
-
-                    {/* <!-- Collapsible wrapper --> */}
-                    <div class="collapse navbar-collapse" id="navbarButtonsExample">
-                        {/* <!-- Left links --> */}
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Dashboard</a>
-                            </li>
-                        </ul>
-                        {/* <!-- Left links --> */}
-
-                        <div class="d-flex align-items-center">
-                            <button type="button" class="btn btn-link px-3 me-2">
-                            Login
-                            </button>
-                            <button type="button" class="btn btn-primary me-3">
-                            Sign up for free
-                            </button>
-                            <a
-                                class="btn btn-dark px-3"
-                                href="https://github.com/mdbootstrap/mdb-ui-kit"
-                                role="button">
-                                <i class="fab fa-github"></i>
-                            </a>
-                        </div>
-                    </div>
-                    {/* <!-- Collapsible wrapper --> */}
-                </div>
-                {/* <!-- Container wrapper --> */}
-            </nav>
-            {/* <!-- Navbar --> */}
-        </>
-    );
-}
-
 export function NavBar() {
     const [clicked, setClicked] = useState(false);;
 
     return (
         <>
-            {/* <!-- Navbar --> */}
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          {/* <!-- Navbar --> */}
+          <nav className="navbar" class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             {/* <!-- Container wrapper --> */}
             <div class="container-fluid">
                 {/* <!-- Toggle button --> */}
@@ -97,7 +32,7 @@ export function NavBar() {
                 </button>
 
                 {/* <!-- Collapsible wrapper --> */}
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="main-navigation collapse navbar-collapse" id="navbarSupportedContent">
                     {/* <!-- Navbar brand --> */}
                     <a class="navbar-brand mt-2 mt-lg-0" href="#">
                         <img
