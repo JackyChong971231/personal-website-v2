5fe236b432ff01c064b14fa00a90c41df91604fc
Jacky <chongkinye@gmail.com>
2024-03-23 18:38:41 -0400

    testing

--- src/config.js
+++
@@ -1,3 +0,0 @@
-const awsApiGatewayUrl = 'https://w7vdsordvd.execute-api.us-east-1.amazonaws.com';
-
-const vmIp = 'http://137.184.166.60:8080';
--- src/service/aws/apiGateway.js
+++ src/service/aws/apiGateway.js
@@ -1,9 +1,11 @@
+import { _config } from "gsap/gsap-core";
+
 const GET   = 'GET';
-const POST  = 'POST';
+const POST  = 'POST'; 
 
 export async function apiGateway(method, endPoint, requestBody = null) {
     // console.log(JSON.stringify(requestBody));
-    const response = await fetch(awsApiGatewayUrl + endPoint, {
+    const response = await fetch(process.env.awsApiGatewayUrl + endPoint, {
         method: method,
         body: JSON.stringify(requestBody)
     })
